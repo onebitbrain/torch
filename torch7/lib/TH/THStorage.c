3ccbcd8870c66de5ac66d90bb48b61ed78a2c5ed
Sam Miller <onebitbrain@users.noreply.github.com>
2015-04-28 18:29:51 -0600

    removed folders

--- torch7/lib/TH/THStorage.c
+++
@@ -1,8 +0,0 @@
-#include "THAtomic.h"
-#include "THStorage.h"
-
-#include "generic/THStorage.c"
-#include "THGenerateAllTypes.h"
-
-#include "generic/THStorageCopy.c"
-#include "THGenerateAllTypes.h"
